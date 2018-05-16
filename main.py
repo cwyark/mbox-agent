d2f5c12ea3efabf38428af3bcf7e58836f891609
ChesterTseng <chester@wylinks.io>
2018-05-16 16:50:48 +0800

    fix typo in BoxAgent main runner

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import logging
 import asyncio
+import serial_asyncio
 import uvloop
 from box import BoxPacketReceiver
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-def __name__ == "__main__":
+if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    coro = serial_asyncio.create_serial_connection(loop, coro, "/dev/ttyS0", baudrate=115200, timeout=1500)
+    coro = serial_asyncio.create_serial_connection(loop, BoxPacketReceiver, "/dev/ttyS0", baudrate=115200, timeout=1500)
     loop.run_until_complete(coro)
     loop.run_forever()
     loop.close()
