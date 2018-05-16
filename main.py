eec9ce66e483e2c46cb62d34f88d19fce81d3e25
ChesterTseng <cwyark@gmail.com>
2018-05-16 16:45:18 +0800

    add asyncio runner in main

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import serial
-import serial_asyncio
 import logging
+import asyncio
+import uvloop
+from box import BoxPacketReceiver
+
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 def __name__ == "__main__":
-    pass
+    loop = asyncio.get_event_loop()
+    coro = serial_asyncio.create_serial_connection(loop, coro, "/dev/ttyS0", baudrate=115200, timeout=1500)
+    loop.run_until_complete(coro)
+    loop.run_forever()
+    loop.close()
