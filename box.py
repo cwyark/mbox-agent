bc2ba54a65ff4900d2fff19b85c6f1d89a899fca
ChesterTseng <chester@wylinks.io>
2018-05-19 08:21:29 +0800

    validate crc

--- box.py
+++ box.py
@@ -2,6 +2,7 @@ import logging
 import asyncio
 import serial_asyncio
 from asyncio import Queue
+from crc import crc
 
 # Set up the logging subsystem
 logging.basicConfig(level=logging.DEBUG, 
@@ -32,13 +33,21 @@ class BoxPacket:
     @classmethod
     def builder(cls, zigbee_id, device_id, counter, payload=b'\x00\x00'):
         buffer = bytearray(12)
+        # start bytes 0xaa 0xd1 (byte 0 ~ 1)
         buffer[0:2] = b'\xaa\xd1'
+        # zigbee id byte 2 ~ 3
         buffer[2:4] = zigbee_id
+        # total bytes  byte 4 (total byte 1B + zigbee id 4B + device id 4B + payload 2 ~ 128B) & 0xFF
         buffer[4:4] = ((1+4+4+len(payload)+2)&0xFF).to_bytes(1, byteorder='little')
+        # device id byte 5 ~ 8
         buffer[5:9] = device_id
+        # counter byte 9 ~ 12 
         buffer[9:13] = counter.to_bytes(4, byteorder='little')
+        # payload start from byte 13
         buffer[13:13] = payload
-        buffer += (1024).to_bytes(2, byteorder='little')
+        # crc 
+        buffer += (crc(buffer[4:])).to_bytes(2, byteorder='little')
+        # end bytes
         buffer += b'\x0d\x55'
         return cls(buffer)
 
@@ -66,6 +75,9 @@ class BoxPacket:
     def crc(self):
         return int.from_bytes(self.msg[-4:-2], byteorder='little')
 
+    def crc_validate(self):
+        return (self.crc == crc(self.msg[4:-4]))
+
     @property
     def payload(self):
         data = self.msg[13:-4]
@@ -87,8 +99,6 @@ class BoxPacketReceiver(asyncio.Protocol):
         self.logger = logging.getLogger('box.BoxPacketReceiver')
         self.logger.info("Connection made")
         self.transport = transport
-        self.queue = Queue()
-        asyncio.ensure_future(self.dispatch_packet_worker())
     def data_received(self, data):
         self.buffer += data
         if b'\x55' in data:
@@ -96,7 +106,6 @@ class BoxPacketReceiver(asyncio.Protocol):
                 self.logger.debug(self.buffer)
                 box_packet = BoxPacket(self.buffer)
                 self.logger.info(box_packet)
-                self.queue.put_nowait(box_packet)
                 self.response(box_packet)
             else:
                 self.logger.info("frame error")
@@ -109,10 +118,6 @@ class BoxPacketReceiver(asyncio.Protocol):
         logger = logging.getLogger('box.response')
         code = packet.command_code
         if code == 1002:
-            response_paket = BoxPacket.builder(zigbee_id = packet.zigbee_id, device_id = packet.device_id, counter = packet.counter, payload=b'\xaa\xbb\xcc\xdd')
-            logger.debug(response_paket)
-            logger.debug(response_paket.msg)
+            response_paket = BoxPacket.builder(zigbee_id = packet.zigbee_id, device_id = packet.device_id, counter = packet.counter, payload=packet.payload)
+            logger.info(response_paket)
 
-    async def dispatch_packet_worker(self):
-        while True:
-            data = await self.queue.get()
