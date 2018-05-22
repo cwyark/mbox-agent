74f21da8cef18955ecc40ded6bc20029a5ea5f32
ChesterTseng <cwyark@gmail.com>
2018-05-22 14:10:08 +0800

    fix string format

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 # Set up the logging system
 logging.basicConfig(level=logging.DEBUG, 
-        format="%(asctime)s %(name)-12s %(levelname)-8s $(message)", 
+        format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s", 
         datefmt="%m-%d %H:%M",
         handlers = [logging.FileHandler('box.log', 'w', 'utf-8'),]
         )
