c6aba5f7845ec9eed73dadec7b435b9186e99a2e
ChesterTseng <chester@wylinks.io>
2018-06-04 14:30:14 +0800

    fix bug when getting zigbee device

--- boxagent/event.py
+++ boxagent/event.py
@@ -49,21 +49,22 @@ async def internet_connection_checker(transport, nic_name):
         global zigbee_device_list_cache
         if _current_conn != _prev_conn:
             now = datetime.now()
-            for zigbee_device, counter in zigbee_device_list_cache.items():
-                payload = Struct("<HBBBBBBBB").pack(1000, \
-                        _int_to_bcd(now.year - 2000), \
-                        _int_to_bcd(now.month), \
-                        _int_to_bcd(now.day), \
-                        _int_to_bcd(now.weekday() + 1), \
-                        _int_to_bcd(now.hour), \
-                        _int_to_bcd(now.minute), \
-                        _int_to_bcd(now.second), \
-                        1)
-                packet = ResponsePacket.builder(zigbee_id = zigbee_device, counter = counter, payload = payload)
-                logger.info("[EVT]<PKTOUT> [CAUSE]<{} status changed> [MSG]<{!s}> [RAW]<{!r}>".format(nic_name, packet, packet))
-                transport.write(packet.frame)
-                zigbee_device_list_cache[zigbee_device] += 1
-            logger.info("[EVT]<NIC> [CAUSE]<none> [MSG]<{} connection {}>".format(nic_name), _current_conn)
+            if len(zigbee_device_list_cache) != 0:
+                for zigbee_device, counter in zigbee_device_list_cache.items():
+                    payload = Struct("<HBBBBBBBB").pack(1000, \
+                            _int_to_bcd(now.year - 2000), \
+                            _int_to_bcd(now.month), \
+                            _int_to_bcd(now.day), \
+                            _int_to_bcd(now.weekday() + 1), \
+                            _int_to_bcd(now.hour), \
+                            _int_to_bcd(now.minute), \
+                            _int_to_bcd(now.second), \
+                            1)
+                    packet = ResponsePacket.builder(zigbee_id = zigbee_device, counter = counter, payload = payload)
+                    logger.info("[EVT]<PKTOUT> [CAUSE]<{} status changed> [MSG]<{!s}> [RAW]<{!r}>".format(nic_name, packet, packet))
+                    transport.write(packet.frame)
+                    zigbee_device_list_cache[zigbee_device] += 1
+                logger.info("[EVT]<NIC> [CAUSE]<none> [MSG]<{} connection {}>".format(nic_name), _current_conn)
         # Do not poll the network so fast! poll it every 100 ms
         await asyncio.sleep(2)
         _prev_conn = _current_conn
