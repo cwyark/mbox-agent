b3913397ef3a699eac836c3826733a65df84a051
ChesterTseng <cwyark@gmail.com>
2018-06-07 17:36:17 +0800

    daemonize and capture signal

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     install_requires = ['uvloop',
         'pyserial',
         'netifaces',
+        'python-daemon',
         'click'],
     classifiers = [
 		'Development Status :: 4 - Beta',
