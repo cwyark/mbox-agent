0a4ba69676799a62b124d79574b389f29092a736
ChesterTseng <cwyark@gmail.com>
2018-06-13 13:16:17 +0800

    update packet format

--- boxagent/packet.py
+++ boxagent/packet.py
@@ -18,25 +18,25 @@ class BasePacket:
         return ' '.join('{:02x}'.format(x) for x in buf)
 
     def __str__(self):
-        return "BasePacket ZigbeeID:0x{zigbee_id:04x} TotalBytes:{total_bytes} DeviceID:{device_id:08x} Counter:{counter} Payload({payload}) CRC:0x{crc:04x}".format(zigbee_id=self.zigbee_id, \
+        return "BasePacket DeviceID:0x{device_id:08x} TotalBytes:{total_bytes} Counter:{counter} Payload({payload}) CRC:0x{crc:04x}".format(device_id=self.device_id, \
                 total_bytes=self.total_bytes, device_id=self.device_id, counter=self.counter, \
                 payload=self.format_bytearray(self.payload), crc=self.crc)
 
     def unpack(self):
-        payload = self.frame[13:-4]
-        data = self.frame[:13] + self.frame[-4:]
+        payload = self.frame[11:-4]
+        data = self.frame[:11] + self.frame[-4:]
         try:
-            self.header_1, self.header_2, self.zigbee_id, self.total_bytes, \
+            self.header_1, self.header_2, self.device_id, self.total_bytes, \
                     self.device_id, self.counter, self.crc, self.end_1, \
-                    self.end_2 = Struct("<BBHBLLHBB").unpack(data)
+                    self.end_2 = Struct("<BBLBLHBB").unpack(data)
         except:
             self.logger.error("[EVT]<PKT> [CAUSE]<payload deserialize not work> [MSG]<none>")
         self.payload = payload
 
     def crc_validate(self):
-        if self.crc != crc(self.frame[4:-4]):
+        if self.crc != crc(self.frame[6:-4]):
             # For some cases, crc value might be as the same as 0D 55
-            if self.crc == crc(self.frame[4:-2]):
+            if self.crc == crc(self.frame[6:-2]):
                 return True
             else:
                 return False
@@ -47,31 +47,30 @@ class BasePacket:
 class RequestPacket(BasePacket):
 
     def __str__(self):
-        return "RequestPacket ZigbeeID:0x{zigbee_id:04x} TotalBytes:{total_bytes} DeviceID:0x{device_id:08x} Counter:{counter} Command:{command} Payload({payload}) CRC:0x{crc:04x}".format(zigbee_id=self.zigbee_id, \
-                total_bytes=self.total_bytes, device_id=self.device_id, counter=self.counter, \
+        return "RequestPacket DeviceID:0x{device_id:08x} TotalBytes:{total_bytes} Counter:{counter} Command:{command} Payload({payload}) CRC:0x{crc:04x}".format(device_id=self.device_id, \
+                total_bytes=self.total_bytes, counter=self.counter, \
                 command=self.command_code, payload=self.format_bytearray(self.payload), crc=self.crc)
 
     @classmethod
-    def builder(cls, zigbee_id, device_id, counter, payload=b'\x00\x00'):
-        total_bytes = (1 + 4 + 4 + len(payload) + 2 ) & 0xFF
-        buffer = bytearray(Struct("<BBHBLLHBB").pack(0xAA, 0xD1, zigbee_id, total_bytes, \
-                device_id, counter, 0x00, 0x0D, 0x55))
+    def builder(cls, device_id, counter, payload=b'\x00\x00'):
+        total_bytes = (1 + 4 + 4 + len(payload)) & 0xFF
+        buffer = bytearray(Struct("<BBLBLHBB").pack(0xAA, 0xD1, device_id, total_bytes, \
+                counter, 0x00, 0x0D, 0x55))
         # Fill up payload
-        buffer[13:13] = payload
+        buffer[11:11] = payload
         # Fill up CRC
         buffer[-4:-2] = crc(buffer[4:-4]).to_bytes(2, byteorder='little')
         return cls(buffer)
 
     def unpack(self):
-        payload = self.frame[13:-4]
-        data = self.frame[:13] + self.frame[-4:]
+        payload = self.frame[11:-4]
+        data = self.frame[:11] + self.frame[-4:]
         try:
-            self.header_1, self.header_2, self.zigbee_id, self.total_bytes, \
-                    self.device_id, self.counter, self.crc, self.end_1, \
-                    self.end_2 = Struct("<BBHBLLHBB").unpack(data)
+            self.header_1, self.header_2, self.device_id, self.total_bytes, \
+                    self.counter, self.crc, self.end_1, \
+                    self.end_2 = Struct("<BBLBLHBB").unpack(data)
             self.payload = payload
             # a workaround
-            self.zigbee_id = int.from_bytes(self.zigbee_id.to_bytes(2, byteorder='little'), byteorder='big')
             self.device_id = int.from_bytes(self.device_id.to_bytes(4, byteorder='little'), byteorder='big')
         except:
             self.logger.error("[EVT]<PKT> [CAUSE]<payload deserialize not work> [MSG]<none>")
@@ -83,34 +82,34 @@ class RequestPacket(BasePacket):
 class ResponsePacket(BasePacket):
 
     def __str__(self):
-        return "ResponsePacket ZigbeeID:0x{zigbee_id:04x} TotalBytes:{total_bytes} Counter:{counter} Command:{command_code} ReplyCode:{reply_code} CRC:0x{crc:04x}".format(zigbee_id=self.zigbee_id, \
+        return "ResponsePacket DeviceID:0x{device_id:08x} TotalBytes:{total_bytes} Counter:{counter} Command:{command_code} ReplyCode:{reply_code} CRC:0x{crc:04x}".format(device_id=self.device_id, \
                 total_bytes=self.total_bytes, counter=self.counter, \
                 command_code=self.command_code, reply_code=self.reply_code, crc=self.crc)
 
     @classmethod
-    def builder(cls, zigbee_id, counter, payload=b'\x00\x00'):
+    def builder(cls, device_id, counter, payload=b'\x00\x00'):
         # A workaround
-        zigbee_id = int.from_bytes(zigbee_id.to_bytes(2, byteorder='little'), byteorder='big')
+        device_id = int.from_bytes(device_id.to_bytes(4, byteorder='little'), byteorder='big')
 
-        total_bytes = (1 + 4 + len(payload) + 2 ) & 0xFF
-        buffer = bytearray(Struct("<BBHBLHBB").pack(0xAA, 0xD1, zigbee_id, total_bytes, \
+        total_bytes = (1 + 4 + len(payload)) & 0xFF
+        buffer = bytearray(Struct("<BBLBLHBB").pack(0xAA, 0xD1, device_id, total_bytes, \
                     counter, 0x00, 0x0D, 0x55))
         # Fill up payload
-        buffer[9:9] = payload
+        buffer[11:11] = payload
         # Fill up CRC
         buffer[-4:-2] = crc(buffer[4:-4]).to_bytes(2, byteorder='little')
         return cls(buffer)
 
     def unpack(self):
-        payload = self.frame[9:-4]
-        data = self.frame[:9] + self.frame[-4:]
+        payload = self.frame[11:-4]
+        data = self.frame[:11] + self.frame[-4:]
         try:
-            self.header_1, self.header_2, self.zigbee_id, self.total_bytes, \
+            self.header_1, self.header_2, self.device_id, self.total_bytes, \
                     self.counter, self.crc, self.end_1, \
-                    self.end_2 = Struct("<BBHBLHBB").unpack(data)
+                    self.end_2 = Struct("<BBLBLHBB").unpack(data)
             self.payload = payload
             # A workaround
-            self.zigbee_id = int.from_bytes(self.zigbee_id.to_bytes(2, byteorder='little'), byteorder='big')
+            self.device_id = int.from_bytes(self.device_id.to_bytes(4, byteorder='little'), byteorder='big')
         except:
             self.logger.error("[EVT]<PKT> [CAUSE]<payload deserialize not work> [MSG]<none>")
 
