436b1656e048328154a1e65dffe435cf112fbffe
ChesterTseng <cwyark@gmail.com>
2018-06-15 12:06:47 +0800

    fix command_code parse

--- boxagent/packet.py
+++ boxagent/packet.py
@@ -21,6 +21,10 @@ class BasePacket:
         return "BasePacket DeviceID:0x{device_id:08x} TotalBytes:{total_bytes} Counter:{counter} Payload({payload}) CRC:0x{crc:04x}".format(device_id=self.device_id, \
                 total_bytes=self.total_bytes, counter=self.counter, payload=self.format_bytearray(self.payload), crc=self.crc)
 
+    @property
+    def command_code(self):
+        return int.from_bytes(self.payload[0:2], byteorder='little')
+
     @classmethod
     def builder(cls, device_id, counter, payload=b'\x00\x00'):
         total_bytes = (1 + 4 + 4 + len(payload) + 2) & 0xFF
