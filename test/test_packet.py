19babba632b48cc276e55ad6f47ffc918a884275
ChesterTseng <cwyark@gmail.com>
2018-06-21 21:32:57 +0800

    update test_packet.py

--- test/test_packet.py
+++ test/test_packet.py
@@ -1,8 +1,25 @@
+import pytest
+import boxagent
 from boxagent.packet import BasePacket
 
-def test_unpack():
-    frame = bytearray(b'\xaa\xd1\x12\x34\x56\x78\x0d\xf5\x31\x00\x00\x22\x0c\x02\x00\x00\x00\x6f\x15\xd0\x55')
+@pytest.fixture
+def frame():
+    return bytearray(b'\xaa\xd1\x12\x34\x56\x78\x0d\xf5\x31\x00\x00\x22\x0c\x02\x00\x00\x00\x6f\x15\xd0\x55')
+
+def test_unpack(frame):
     try:
         packet = BasePacket(frame)
     except:
         assert 0
+
+def test_crc_validate(frame):
+    try:
+        packet = BasePacket(frame)
+    except:
+        assert 0
+
+    assert packet.crc_validate() == False
+
+def test_builder():
+    # Should test builder, but classmethod not work 
+    assert True
