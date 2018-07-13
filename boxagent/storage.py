29727fbbdee5469ccddf07d6deb07198c2526779
ChesterTseng <cwyark@gmail.com>
2018-07-13 17:43:37 +0800

    fix MboxId show

--- boxagent/storage.py
+++ boxagent/storage.py
@@ -24,19 +24,22 @@ class StorageRunner:
                    self.file_counter
                    )
                 q_list = list()
+                if self.queue.empty() is not True:
+                    self.file_counter += 1
                 while self.queue.empty() is not True:
                     q = self.queue.get_nowait()
+                    q['MBoxId'] = "0x{:x}".format(q['MBoxId'])
                     self.logger.debug(q)
                     q_list.append(q)
-                with open(os.path.join(storage_path_prefix, file_name), 'a+') as f:
-                    if storage_type == 'json':
-                        f.write(json.dumps(q_list))
-                    elif storage_type == 'sql':
-                        for stmt in q_list:
-                            attr = "({}, {}, {}, {}, {})".format(*stmt.keys())
-                            values = "('{:x}', '{}', '{}', '{}', '{}')".format(*stmt.values())
-                            sql_stmt = "INSERT INTO {} VALUES {}\n".format(attr, values)
-                            self.logger.debug(sql_stmt)
-                            f.write(sql_stmt)
-                    else:
-                        pass
+                    with open(os.path.join(storage_path_prefix, file_name), 'a+') as f:
+                        if storage_type == 'json':
+                            f.write(json.dumps(q_list))
+                        elif storage_type == 'sql':
+                            for stmt in q_list:
+                                attr = "({}, {}, {}, {}, {})".format(*stmt.keys())
+                                values = "('{:x}', '{}', '{}', '{}', '{}')".format(*stmt.values())
+                                sql_stmt = "INSERT INTO {} VALUES {}\n".format(attr, values)
+                                self.logger.debug(sql_stmt)
+                                f.write(sql_stmt)
+                        else:
+                            pass
