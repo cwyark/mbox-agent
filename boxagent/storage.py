e283e25aa0b90bd078b36060a1bacfbb148a1f7f
ChesterTseng <cwyark@gmail.com>
2018-07-23 10:13:50 +0800

    filter out no data

--- boxagent/storage.py
+++ boxagent/storage.py
@@ -24,21 +24,21 @@ class StorageRunner:
                    self.file_counter
                    )
                 q_list = list()
-                if self.queue.empty() is not True:
-                    self.file_counter += 1
                 while self.queue.empty() is not True:
                     q = self.queue.get_nowait()
                     q['MBoxId'] = "0x{:x}".format(q['MBoxId'])
                     self.logger.info(q)
                     q_list.append(q)
-                with open(os.path.join(storage_path_prefix, file_name), 'a+') as f:
-                    if storage_type == 'json':
-                        f.write(json.dumps(q_list))
-                    elif storage_type == 'sql':
-                        for stmt in q_list:
-                            attr = "({}, {}, {}, {}, {})".format(*stmt.keys())
-                            values = "('{:x}', '{}', '{}', '{}', '{}')".format(*stmt.values())
-                            sql_stmt = "INSERT INTO {} VALUES {}\n".format(attr, values)
-                            f.write(sql_stmt)
-                    else:
-                        self.logger.info("Unsupport Storage method");
+                if len(q_list) != 0:
+                    self.file_counter += 1
+                    with open(os.path.join(storage_path_prefix, file_name), 'a+') as f:
+                        if storage_type == 'json':
+                            f.write(json.dumps(q_list))
+                        elif storage_type == 'sql':
+                            for stmt in q_list:
+                                attr = "({}, {}, {}, {}, {})".format(*stmt.keys())
+                                values = "('{:x}', '{}', '{}', '{}', '{}')".format(*stmt.values())
+                                sql_stmt = "INSERT INTO {} VALUES {}\n".format(attr, values)
+                                f.write(sql_stmt)
+                        else:
+                            self.logger.info("Unsupport Storage method");
