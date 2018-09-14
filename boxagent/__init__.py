66c4e80b0cbf275aabfb92feeafb658579c85bd4
YuSheng <hello@cwyark.me>
2018-09-14 11:23:00 +0800

    rename event to internety

--- boxagent/__init__.py
+++ boxagent/__init__.py
@@ -1,6 +1,6 @@
 from . import packet
 from . import crc
 from . import core
-from . import event
+from . import internet
 from . import serial_transport
 from . import storage
--- boxagent/internet.py
+++ boxagent/internet.py
@@ -5,20 +5,6 @@ from .packet  import BasePacket
 from datetime import datetime
 from struct import Struct, pack, unpack
 
-def _int_to_bcd(n):
-    """
-    Encode a one or two digits number to the BCD.
-
-    """
-    bcd = 0
-    for i in (n // 10, n % 10):
-    	for p in (8, 4, 2, 1):
-            if i >= p:
-                bcd += 1
-                i -= p
-            bcd <<= 1
-    return bcd >> 1
-
 async def internet_connection_checker(storage_queue, nic_name):
     logger = logging.getLogger(__name__)
     UP = 1
