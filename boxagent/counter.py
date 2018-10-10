480d03f9bb3d983eb9a13dcc578ccf8c4502e7e0
ChesterTseng <hello@cwyark.me>
2018-10-10 17:01:14 +0800

    add led blink in counter

--- boxagent/counter.py
+++ boxagent/counter.py
@@ -3,11 +3,11 @@ import asyncio
 from datetime import datetime
 from .pin import *
 
-
 async def fast_counter_detect (loop, storage_queue, sampling_rate):
     logger = logging.getLogger(__name__)
     queue = storage_queue
     pin = FAST_COUNTER
+    led = FAST_COUNTER_LED
     _prev = counter_value(pin)
     _value = 0
     _counting = 0
@@ -18,11 +18,25 @@ async def fast_counter_detect (loop, storage_queue, sampling_rate):
         if _prev == 0 and _value == 1:
             _counting += 1
         _prev = _value
+        now = datetime.utcnow()
+        if now.second == 0:
+            led_on(led)
+            await asyncio.sleep(1)
+            logger.info("FAST counter collection: {}".format(_counting))
+            q = dict()
+            q['EventCode'] = 3201
+            q['RecordDate'] = now.strftime('%Y-%m-%d %H:%M:%S.%f')
+            q['SequentialNumber'] = _seq_num
+            q['Value'] = _counting
+            await queue.put(q)
+            _seq_num += 1
+            led_off(led)
 
 async def direct_counter_detect (loop, storage_queue, sampling_rate):
     logger = logging.getLogger(__name__)
     queue = storage_queue
     pin = DIRECT_COUNTER
+    led = DIRECT_COUNTER_LED
     _prev = counter_value(pin)
     _value = 0
     _counting = 0
@@ -31,10 +45,14 @@ async def direct_counter_detect (loop, storage_queue, sampling_rate):
         await asyncio.sleep(sampling_rate)
         _value = counter_value(pin)
         if _prev == 0 and _value == 1:
+            led_on(led)
             q = dict()
             q['Eventcode'] = 3106
             q['SequentialNumber'] = _seq_num
             q['RecordDate'] = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
             await queue.put(q)
             _seq_num += 1
+            logger.info("Direct counter triggered")
+            await asyncio.sleep(1)
+            led_off(led)
         _prev = _value
