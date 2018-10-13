e389ed6b195aa1f0a52410086f48ee6909fdabae
ChesterTseng <hello@cwyark.me>
2018-10-13 17:29:26 +0800

    add rsyslog config file and copy to the confi path:Wq

--- setup.py
+++ setup.py
@@ -1,17 +1,26 @@
 from setuptools import setup, find_packages
-from setuptools.command.install import install
+from setuptools.command.install_scripts import install_scripts
 import os
+import shutil
 
 def read(relpath: str) -> str:
     with open(os.path.join(os.path.dirname(__file__), relpath)) as f:
         return f.read()
 
-class CustomInstallCommand(install):
+class CustomInstallCommand(install_scripts):
     """Customized setuptools install command"""
 
     def run(self):
-        install.run(self)
-        os.mkdir("/etc/datalogger")
+        RSYSLOG_PATH = "/etc/rsyslog.d"
+        CONFIG_PATH = "/etc/datalogger"
+        SYSTEMD_PATH = "/etc/systemd/system"
+        install_scripts.run(self)
+        if not os.path.exists(CONFIG_PATH):
+            os.mkdir(CONFIG_PATH)
+        shutil.copy2("config.ini", CONFIG_PATH)
+        shutil.copy2("configs/datalogger.service", SYSTEMD_PATH)
+        if os.path.exists(RSYSLOG_PATH):
+            shutil.copy2("configs/datalogger.rsyslog.conf", RSYSLOG_PATH)
 
 setup(
     name = "DataLogger",
@@ -23,9 +32,10 @@ setup(
     license = 'Apache 2.0',
     packages = find_packages(),
     scripts = ['loggerd'],
+    include_package_data = True,
     data_files = [
         ('/etc/systemd/system', ['configs/datalogger.service']), 
-        ('', ['config.ini'])
+        ('/etc/datalogger', ['config.ini'])
     ],
     install_requires = ['uvloop',
         'configobj',
@@ -40,7 +50,7 @@ setup(
 		'Programming Language :: Python :: 3.6',
 	],
     cmdclass = {
-        'install': CustomInstallCommand
+        'install_scripts': CustomInstallCommand
     },
     setup_requires = ['pytest-runner', 'setuptools-git-version'],
     tests_require = ['pytest']
