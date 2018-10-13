3d5516d61fc5bd7e5b7f6c9d0a2f71c85c00070b
ChesterTseng <cwyark@gmail.com>
2018-10-13 09:52:30 +0800

    rename module name to datalogger and add custom script when using pip install

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 import os
 
 def read(relpath: str) -> str:
     with open(os.path.join(os.path.dirname(__file__), relpath)) as f:
         return f.read()
 
+class CustomInstallCommand(install):
+    """Customized setuptools install command"""
+
+    def run(self):
+        install.run(self)
+
 setup(
     name = "DataLogger",
     version_format = '{tag}+{gitsha}',
     description = 'Data Logger is an asynchronous data collector and recorder.',
     long_description = read('README.md'),
-    author = 'ChesterTseng',
+    author = 'YuSheng T.',
     author_email = 'hello@cwyark.me',
     license = 'Apache 2.0',
     packages = find_packages(),
@@ -29,6 +36,9 @@ setup(
 		'Programming Language :: Python :: 3 :: Only',
 		'Programming Language :: Python :: 3.6',
 	],
+    cmdclass = {
+        'install': CustomInstallCommand
+    },
     setup_requires = ['pytest-runner', 'setuptools-git-version'],
     tests_require = ['pytest']
 )
