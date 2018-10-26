a579e3fa13a122bae648072c10885ab0af4dd11b
YuSheng T <hello@cwyark.me>
2018-10-26 05:41:19 +0000

    fix rc522 driver

--- datalogger/rc522.py
+++ datalogger/rc522.py
@@ -8,8 +8,8 @@ async def RC522Runner (loop, storage_queue):
     logger = logging.getLogger(__name__)
     reader = MFRC522.MFRC522()
     while True:
-        (status, tag_type) = reader.MRFC522_REquest(reader.PICC_REQIDL)
-        (status, uid) = reader.MRFC522_Anticoll()
+        (status, tag_type) = reader.MFRC522_Request(reader.PICC_REQIDL)
+        (status, uid) = reader.MFRC522_Anticoll()
         if status == reader.MI_OK:
             uid_number = (uid[3] << 24) + (uid[2] << 16) + (uid[1] << 8) + uid[0]
             q = dict()
@@ -17,4 +17,4 @@ async def RC522Runner (loop, storage_queue):
             q['RecordDate'] = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
             q['rfid1'] = uid_number
             await storage_queue.put(q)
-        await asyncio.sleep(0.5)
+        await asyncio.sleep(2)
