48a418aa14b6564a54be2cd9a1cca2ca908a7886
YuSheng <hello@cwyark.me>
2018-11-08 08:33:53 +0800

    add workaround for rc522y

--- datalogger/rc522.py
+++ datalogger/rc522.py
@@ -8,6 +8,10 @@ async def RC522Runner (loop, storage_queue):
     logger = logging.getLogger(__name__)
     reader = MFRC522.MFRC522()
     led_off(RC522_DETECT_LED)
+    await asyncio.sleep(5)
+    # a workaround, I don't know why the first time run, rc522 can not work, it needs reset again
+    del reader
+    reader = MFRC522.MFRC522()
     while True:
         (status, tag_type) = reader.MFRC522_Request(reader.PICC_REQIDL)
         (status, uid) = reader.MFRC522_Anticoll()
