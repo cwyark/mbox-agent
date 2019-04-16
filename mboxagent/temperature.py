48fe8315389ecdb8d11ec8207408adfbfde945fb
YuSheng <chester@cwyark.me>
2019-04-16 08:24:23 +0100

    change event code of temperature

--- mboxagent/temperature.py
+++ mboxagent/temperature.py
@@ -4,7 +4,7 @@ import asyncio
 import logging
 from datetime import datetime
 
-async def TemperatureRunner (loop, storage_queue, cs = SPI_CS, miso = SPI_MISO, mosi = SPI_MOSI, clk = SPI_CLK, temp_no = 1):
+async def TemperatureRunner (loop, storage_queue, cs = SPI_CS, miso = SPI_MISO, mosi = SPI_MOSI, clk = SPI_CLK, event_code = 3500):
     logger = logging.getLogger(__name__)
     _temp = 0
     _prev_temp = 0
@@ -14,12 +14,11 @@ async def TemperatureRunner (loop, storage_queue, cs = SPI_CS, miso = SPI_MISO, 
         await asyncio.sleep(10)
         _max31865.readTemp()
         _temp = round(_max31865.tempC, 1)
-        logger.info("get temperature {} = {}".format(temp_no, _temp))
+        logger.info("get temperature {} = {}".format(event_code, _temp))
         if _temp != _prev_temp:
-            data_attri = "Sensor{}".format(temp_no)
             q = dict()
-            q['EventCode'] = 3500
+            q['EventCode'] = event_code
             q['RecordDate'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-            q[data_attri] = round(_temp * 10)
+            q['value'] = round(_temp * 10)
             await storage_queue.put(q)
         _prev_temp = _temp
